a7f7f5569f6c86a4ca4bfdb515ede03789df3fee
Abner005g <97368172+Abner005g@users.noreply.github.com>
2023-05-17 13:07:03 -0300

    Add files via upload

---
+++ assets/java/main.js
@@ -0,0 +1,19 @@
+$("#mobile").click(function (e) { 
+    
+    $("#nav").toggleClass("active");
+
+});
+
+
+
+$("#cc").click(function (e){
+
+    var cp = $("#ff").val(); 
+    var co = $("#pp").val();
+    var resultado1 = cp  / co;
+    var resultado2 = 1 - resultado1;
+
+    $("#r1").val(resultado1);
+    $("#r2").val(resultado2);
+
+});
